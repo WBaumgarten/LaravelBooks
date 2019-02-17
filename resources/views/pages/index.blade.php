c156215560f66f8f60410f8d74ca935242071fba
WBaumgarten <wbaum.dev@gmail.com>
2019-02-18 00:24:23 +0200

    Changed home screen flavour text

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -10,15 +10,12 @@
     own database of books with their corresponding authors.
     The larger the database, the easier it will be to find some information on
     the book that you've been looking for, or finding more books from authors
-    that you love. Feel free to add new books to the database, or edit and
-    remove incorrect entries.
+    that you love.
 </p>
 <p>
+    After you register an account, feel free to add new books to the database, or edit and
+    remove incorrect entries.
     Since LaravelBook's data is managed by volunteers, it is the community's
     responsibility to make sure that correct information is entered.
-</p>
-<p>
-    And remember, removing all of the books might be funny for a second, but
-    each book deleted causes a baby kitten to cry.
 </p>
     @endsection
